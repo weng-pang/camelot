f998bdcb218fc44fea58e7f8ab232b1f0f390464
Peter Serwylo <peter@serwylo.com>
2018-05-10 16:48:17 +1000

    Added more meaningful content to the database

--- config/database.sql
+++ config/database.sql
@@ -55,7 +55,7 @@ CREATE TABLE settings (
   PRIMARY KEY (id)
 );
 
-INSERT INTO settings (title, subtitle) VALUES ('Camelot', 'A CMS written in CakePHP for the IE project.');
+INSERT INTO settings (title, subtitle) VALUES ('Camelot', 'A modern CMS written in CakePHP, do demonstrate various development practices.');
 
 -- The password for this user is "root@example.com" (before being hashed).
 INSERT INTO users (email, password, created, modified)
@@ -65,12 +65,14 @@ VALUES
 INSERT INTO articles (user_id, title, slug, body, published, created, modified)
 VALUES
 (1, 'Welcome to Camelot', 'welcome', 'Camelot is a custom CMS written using CakePHP. The idea is to show how various different functionality can be implemented in a modern PHP website using the CakePHP framework. Initially, it is a basic blog site, but over time it will grow as more features are added. The source code can be viewed at https://git.infotech.monash.edu/UGIE/cms.', 1, now(), now()),
-(1, 'Second Post', 'second-post', 'This is the second post.', 1, now(), now()),
-(1, 'Third Post', 'third-post', 'This is the third post.', 1, now(), now()),
+(1, 'Viewing the source code of Camelot', 'viewing-source', 'As this website is built to illustrate certain coding practices, it is important to be able to check out the source code of the website. This includes not only the PHP code (Controllers, Models, etc), but also the view code (Views, Layouts). On any page, you should be able to view links to the relevant Controller, Action, View, and Template for that page. This is true both for the public website, and also the administrator dashaboard. In addition to this, the entire source code is available at https://git.infotech.monash.edu/UGIE/cms.', 1, now(), now()),
+(1, 'Logging in to the Admin Dashboard', 'admin-dashboard', 'Anyone can log into the administrator dashboard for this website. This site is currently configured to be in "demo mode", a mode I invented which allows anyone to log in and play around with the administrator dashboard. To do this, click the "Login" button in the top right of any page. As it is in demo mode, it is already prefilled with a username + password that you can use to log in. Any content you add will periodically get deleted as the website database is cleared and reinstalled.', 1, now(), now()),
 (1, 'Fourth Post', 'fourth-post', 'This is the fourth post.', 1, now(), now()),
 (1, 'Fifth Post', 'fifth-post', 'This is the fifth post.', 1, now(), now()),
 (1, 'Sixth Post', 'sixth-post', 'This is the sixth post.', 1, now(), now());
 
+-- Insert some dummy data about people viewing articles, so that we can see appropriate charts in the admin dashboard,
+-- and it will also be able to show the most popular articles.
 INSERT INTO article_views (user_id, article_id, created, modified)
 VALUES
 (1, 1, now(), now()),
